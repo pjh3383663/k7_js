0f64ec62aa30140c39c5a50cda1e00404efb75d8
pjh3383663 <pjh3383663@naver.com>
2024-05-03 16:37:16 +0900

    컴포넌트 모듈 css

---
+++ src/01/Hello.js
@@ -0,0 +1,15 @@
+function Hello() {
+    return (
+        <>
+            <p>
+                K-digital 7기 박준형
+            </p>
+            <ul>
+                <li>리액트 컴포넌트</li>
+            </ul>
+        </>
+        
+    );
+}
+
+export default Hello;
---
+++ src/02/MyClockImage.js
@@ -0,0 +1,11 @@
+import cimg from './colock.png' 
+
+function MyClockImage() {
+    return (
+        <div>
+            <img src={cimg} className='App-logo' alt = '시계'/>
+        </div>
+    );
+}
+
+export default MyClockImage;
